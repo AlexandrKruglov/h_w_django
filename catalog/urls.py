534e48a274f46c898ab19e64b686d2c3d35f66f1
AlexandrKruglov <krulalex84@gmail>
2024-05-21 00:43:44 +0300

    созданы шаблоны и подшаблоны

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts
-
+from catalog.views import home, contacts, product, product_detal
 app_name = CatalogConfig.name
 urlpatterns = [
-    path('', home, name='hone'),
+    path('', home, name='home'),
     path('contacts/', contacts, name='contacts'),
+    path('product/', product, name='product'),
+    path('product/<int:pk>/', product_detal, name='product_detal')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 import os
+
+from catalog.models import Product
+
 
 def home(request):
     return render(request, 'home.html')
@@ -13,3 +16,15 @@ def contacts(request):
         with open(os.path.join("cont.txt"), 'a', encoding="utf-8") as f:
             f.write(f"nane: {name}, phone: {phone}, message: {message}")
     return render(request, 'contacts.html')
+
+
+def product(request):
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'product_list.html', context)
+
+
+def product_detal(request, pk):
+    one_product = Product.objects.get(pk=pk)
+    context = {'one_product': one_product}
+    return render(request, 'product_detal', context)
